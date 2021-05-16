9bd28d178a84090a3fad8e449771d521d97bff05
WiebedeBoer <wiebe81@gmail.com>
2021-05-16 10:11:24 +0200

    i inc

--- tekenprogramma/Visitor.cs
+++ tekenprogramma/Visitor.cs
@@ -483,6 +483,30 @@ namespace tekenprogramma
                         }
                     }
                 }
+                CheckInSubgroup(group, element.AccessKey);
+            }
+            return counter;
+        }
+
+        public int CheckInSubgroup(Group group, string key)
+        {
+            int counter = 0;
+            if (group.drawnElements.Count() > 0)
+            {
+                foreach (FrameworkElement groupelement in group.drawnElements)
+                {
+                    if (groupelement.AccessKey == key)
+                    {
+                        counter++;
+                    }
+                }
+            }
+            if (group.addedGroups.Count() > 0)
+            {
+                foreach (Group subgroup in group.addedGroups)
+                {
+                    counter = subgroup.CheckInSubgroup(subgroup, key);
+                }
             }
             return counter;
         }
@@ -497,6 +521,7 @@ namespace tekenprogramma
             while (i < depth)
             {
                 str += "\t";
+                i++;
             }
             int groupcount = group.drawnElements.Count() + group.addedGroups.Count();
             str = str + "group " + groupcount + "\n";
